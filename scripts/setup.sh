d8b287ca78dbd2f0a62b46fada5c53b46a8a3da1
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-08-02 22:06:30 +0530

    [dotfiles] fix: kitty config update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,27 +3,36 @@
 echo "Setting up dotfiles..."
 
 # symlinks
+
 # zsh
 ln -sf "$HOME/dotfiles/zsh/plugins.txt" "$HOME/.zsh_plugins.txt" # antidote .zsh_plugins.txt file
 
 mkdir -p "$HOME/.config"
 
-# starship
-ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml # starship config file
-
 # git
+rm -rf ~/.config/git
 ln -sfn "$HOME/dotfiles/git" "$HOME/.config/git"
 
 # mise
+rm -rf ~/.config/mise
 ln -sfn "$HOME/dotfiles/mise" "$HOME/.config/mise"
 
 # kitty
+rm -rf ~/.config/kitty
 ln -sfn "$HOME/dotfiles/kitty" "$HOME/.config/kitty"
 
+# alacritty
+rm -rf ~/.config/alacritty
+ln -sfn "$HOME/dotfiles/alacritty" "$HOME/.config/alacritty"
+
 # nvim
+rm -rf ~/.config/nvim
 ln -sfn "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 
 # aerospace
 ln -sf ~/dotfiles/aerospace.toml ~/.aerospace.toml
+
+# starship
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml # starship config file
 
 echo "Done."
