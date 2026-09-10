bc7642b4a741155ab0aeee97bb28ded93c657f6a
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-09-10 15:36:27 +0530

    [dotfiles] feat: git related automations

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -14,3 +14,32 @@ emcc_setup() {
   export EMSDK_BIN="$root/AJS-emcc/$version/emsdk/upstream/bin"
   export PATH="$EMSDK_BIN:$PATH"
 }
+
+# git related automations
+
+# git branch switch using fzf
+gcb() {
+  local branch
+  branch=$(git branch | SHELL=/bin/sh fzf | tr -d '[:space:]')
+
+  if [[ -n "$branch" ]]; then
+    git checkout "${branch#* }" # strips the "*" prefix if it's the current branch
+  fi
+}
+
+# multi branch deletion
+gbd() {
+    local branches
+
+    branches=$(
+        git branch --format='%(refname:short)' |
+        grep -v "^$(git branch --show-current)$" |
+        fzf --multi \
+            --height=60% \
+            --prompt='Delete branches > ' \
+            --preview='git log --oneline --decorate -10 {}' \
+            --preview-window=right:65%:wrap
+    )
+
+    [[ -n "$branches" ]] && echo "$branches" | xargs git branch -D
+}
--- zsh/main.zsh
+++ zsh/main.zsh
@@ -10,6 +10,6 @@ source ~/dotfiles/zsh/exports.zsh
 source ~/dotfiles/zsh/functions.zsh
 
 # prompt
-eval "$(zoxide init zsh)"
 eval "$(mise activate zsh)"
 eval "$(starship init zsh)"
+eval "$(zoxide init zsh)"
